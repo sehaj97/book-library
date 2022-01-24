ba6b9c03b59f69daa513b24b6c477ab490cd016d
sehaj97 <sehajmagan@gmail.com>
2022-01-24 13:29:43 -0500

    Added Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import {
+  ApolloClient,
+  ApolloProvider
+} from "@apollo/client";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+    request: operation => {
+        const token = localStorage.getItem('id_token');
+
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
+    },
+    uri: '/graphql'
+});
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path='/' component={SearchBooks} />
+                        <Route exact path='/saved' component={SavedBooks} />
+                        <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+                    </Switch>
+                </>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
